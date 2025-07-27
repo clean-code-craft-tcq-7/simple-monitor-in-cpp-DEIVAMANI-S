775c14130c869b933f7bfe0849cbf335541123e1
qei1cob <qei1cob@bosch.com>
2025-07-27 18:02:56 +0530

    Code update as per Clean Coding Principles
    Don’t Repeat Yourself , Single Responsibility Principle & readability

--- monitor.cpp
+++ monitor.cpp
@@ -5,34 +5,46 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
+//Vital Range limits
+constexpr float max_temperature = 102.0f;
+constexpr float min_temperature = 95.0f;
+constexpr int max_pulse_rate = 100;
+constexpr int min_pulse_rate = 60;
+constexpr int min_spo2_level = 90;
+
+// Blinking alert duration
+constexpr int alertcount = 6;
+
+// Function to display (or) issue alert
+void DisplayAlert()
+{
+    for (int i = 0; i < alertcount; i++) 
+    {
       cout << "\r* " << flush;
       sleep_for(seconds(1));
       cout << "\r *" << flush;
       sleep_for(seconds(1));
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+}
+
+// Function to check a single vital parameter
+bool CheckVitalsRange(const char* vitalsName, float readingValue, float minlimit, float maxlimit)
+{
+    if (readingValue < minlimit || readingValue > maxlimit) 
+    {
+        cout << vitalsName << " is Critical!!!\n";
+        DisplayAlert();
+        return false;
     }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
+    return true;
+}
+
+
+int IsVitalsOk(float temperature, float pulseRate, float spo2) 
+{
+    bool tempOk = CheckVitalsRange("Temperature", temperature, min_temperature, max_temperature);
+    bool pulseOk = CheckVitalsRange("Pulse Rate", pulseRate, min_pulse_rate, max_pulse_rate);
+    bool spo2Ok = CheckVitalsRange("Oxygen Saturation", spo2, min_spo2_level, 100); // 100 is user creted upper-bound for reference 
+
+    return (tempOk && pulseOk && spo2Ok) ? 1 : 0;
 }
--- monitor.h
+++ monitor.h
@@ -1,3 +1,3 @@
 #pragma once
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+int IsVitalsOk(float temperature, float pulseRate, float spo2) 
