fb2ce99d0aba247dc8637f26cd9e9243215433d1
qei1cob <qei1cob@bosch.com>
2025-07-28 06:43:03 +0530

    UT test for Clean coding updated code
    UT test for Clean coding updated code

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,44 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+// Override DisplayAlert to avoid sleep in tests
+void DisplayAlert()
+{
+    // Do nothing for tests
+}
+
+// ----------- Tests for CheckVitalsRange ------------
+
+TEST(CheckVitalsRangeTest, ReturnsTrueForValidReading) {
+    EXPECT_TRUE(CheckVitalsRange("Temperature", 98.6, min_temperature, max_temperature));
+}
+
+TEST(CheckVitalsRangeTest, ReturnsFalseForLowReading) {
+    EXPECT_FALSE(CheckVitalsRange("Temperature", 90.0, min_temperature, max_temperature));
+}
+
+TEST(CheckVitalsRangeTest, ReturnsFalseForHighReading) {
+    EXPECT_FALSE(CheckVitalsRange("Temperature", 105.0, min_temperature, max_temperature));
+}
+
+// ----------- Tests for IsVitalsOk ------------
+
+TEST(IsVitalsOkTest, AllVitalsNormal) {
+    EXPECT_EQ(IsVitalsOk(98.6, 72, 95), 1);
+}
+
+TEST(IsVitalsOkTest, TemperatureTooHigh) {
+    EXPECT_EQ(IsVitalsOk(103.0, 72, 95), 0);
+}
+
+TEST(IsVitalsOkTest, PulseTooLow) {
+    EXPECT_EQ(IsVitalsOk(98.6, 50, 95), 0);
+}
+
+TEST(IsVitalsOkTest, Spo2TooLow) {
+    EXPECT_EQ(IsVitalsOk(98.6, 72, 85), 0);
+}
+
+TEST(IsVitalsOkTest, MultipleVitalsOutOfRange) {
+    EXPECT_EQ(IsVitalsOk(104.0, 50, 85), 0);
 }
