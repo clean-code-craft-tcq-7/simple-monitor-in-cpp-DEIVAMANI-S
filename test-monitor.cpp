8fba4cd8bbbc5459253a715ad74c4f05e864de67
qei1cob <qei1cob@bosch.com>
2025-07-29 10:42:48 +0530

    [Cleancoding]-UT and lint fixes
    [Cleancoding]-UT and lint fixes

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -5,3 +5,25 @@ TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(isvitalsOk(99, 102, 70));
   ASSERT_TRUE(isvitalsOk(98.1, 70, 98));
 }
+
+// ----------- Tests for IsVitalsOk ------------
+
+TEST(IsVitalsOkTest, AllVitalsNormal) {
+    EXPECT_EQ(isvitalsOk(98.6, 72, 95), 1);
+}
+
+TEST(IsVitalsOkTest, TemperatureTooHigh) {
+    EXPECT_EQ(isvitalsOk(103.0, 72, 95), 0);
+}
+
+TEST(IsVitalsOkTest, PulseTooLow) {
+    EXPECT_EQ(isvitalsOk(98.6, 50, 95), 0);
+}
+
+TEST(IsVitalsOkTest, Spo2TooLow) {
+    EXPECT_EQ(isvitalsOk(98.6, 72, 85), 0);
+}
+
+TEST(IsVitalsOkTest, MultipleVitalsOutOfRange) {
+    EXPECT_EQ(isvitalsOk(104.0, 50, 85), 1);
+}
